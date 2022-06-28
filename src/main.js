b7fb20c0314f0cdc1ea10adec6410711e404d8fd
why <1790279527@qq.com>
2022-06-28 16:16:12 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入自定义样式
+import '@/styles/index.less'
+
+// 引入vant组件
+import {
+    Button
+} from 'vant'
+
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
